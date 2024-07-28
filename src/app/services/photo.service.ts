e0b3ac37c7372150b9173f1c8f789554925e1af8
Ben <bennyperdana.jakarta@gmail.com>
2024-07-28 21:45:45 +0700

    Tab 2 - add photo gallery

---
+++ src/app/services/photo.service.ts
@@ -0,0 +1,165 @@
+import { Injectable } from '@angular/core';
+import {
+  Camera,
+  CameraResultType,
+  CameraSource,
+  Photo,
+} from '@capacitor/camera';
+import { Filesystem, Directory } from '@capacitor/filesystem';
+import { UserPhoto } from '../model/UserPhoto';
+import { Preferences } from '@capacitor/preferences';
+import { Platform } from '@ionic/angular';
+import { Capacitor } from '@capacitor/core';
+
+@Injectable({
+  providedIn: 'root',
+})
+export class PhotoService {
+  public photos: UserPhoto[] = [];
+  private PHOTO_STORAGE: string = 'photos';
+  private platform: Platform;
+
+  constructor(platform: Platform) {
+    this.platform = platform;
+  }
+
+  public async addNewToGallery(): Promise<void> {
+    try {
+      // Take a photo
+      const capturedPhoto = await Camera.getPhoto({
+        resultType: CameraResultType.Uri,
+        source: CameraSource.Camera,
+        quality: 100,
+      });
+
+      // Save the picture and add it to photo collection
+      const savedImageFile = await this.savePicture(capturedPhoto);
+      this.photos.unshift(savedImageFile);
+      await Preferences.set({
+        key: this.PHOTO_STORAGE,
+        value: JSON.stringify(this.photos),
+      });
+    } catch (error) {
+      console.error('Error taking photo', error);
+    }
+  }
+
+  // Save picture to file on device
+  private async savePicture(photo: Photo): Promise<UserPhoto> {
+    // Convert photo to base64 format, required by Filesystem API to save
+    const base64Data = await this.readAsBase64(photo);
+
+    // Mirror the image
+    const mirroredImage = await this.mirrorImage(base64Data);
+
+    // Write the file to the data directory
+    const fileName = Date.now() + '.jpeg';
+    const savedFile = await Filesystem.writeFile({
+      path: fileName,
+      data: mirroredImage,
+      directory: Directory.Data,
+    });
+
+    if (this.platform.is('hybrid')) {
+      // Display the new image by rewriting the 'file://' path to HTTP
+      // Details: https://ionicframework.com/docs/building/webview#file-protocol
+      return {
+        filepath: savedFile.uri,
+        webviewPath: Capacitor.convertFileSrc(savedFile.uri),
+      };
+    } else {
+      // Use webPath to display the new image instead of base64 since it's
+      // already loaded into memory
+      return {
+        filepath: fileName,
+        webviewPath: photo.webPath,
+      };
+    }
+  }
+
+  private async readAsBase64(photo: Photo): Promise<string> {
+    // "hybrid" will detect Cordova or Capacitor
+    if (this.platform.is('hybrid')) {
+      // Read the file into base64 format
+      const file = await Filesystem.readFile({
+        path: photo.path!,
+      });
+
+      return file.data as string; // In hybrid platforms, the data is always a string
+    } else {
+      // Fetch the photo, read as a blob, then convert to base64 format
+      const response = await fetch(photo.webPath!);
+      const blob = await response.blob();
+
+      return this.convertBlobToBase64(blob);
+    }
+  }
+
+  private convertBlobToBase64 = (blob: Blob): Promise<string> =>
+    new Promise((resolve, reject) => {
+      const reader = new FileReader();
+      reader.onerror = reject;
+      reader.onload = () => {
+        resolve(reader.result as string);
+      };
+      reader.readAsDataURL(blob);
+    });
+
+  private async mirrorImage(base64Str: string): Promise<string> {
+    return new Promise((resolve, reject) => {
+      const img = new Image();
+      img.src = base64Str;
+
+      img.onload = () => {
+        const canvas = document.createElement('canvas');
+        const ctx = canvas.getContext('2d');
+
+        canvas.width = img.width;
+        canvas.height = img.height;
+
+        // Flip the image horizontally
+        if (ctx) {
+          ctx.translate(img.width, 0);
+          ctx.scale(-1, 1);
+
+          ctx.drawImage(img, 0, 0);
+        }
+
+        const mirroredBase64 = canvas.toDataURL('image/jpeg');
+        resolve(mirroredBase64);
+      };
+
+      img.onerror = (err) => reject(err);
+    });
+  }
+
+  public async loadSaved(): Promise<void> {
+    // Retrieve cached photo array data
+    const { value } = await Preferences.get({ key: this.PHOTO_STORAGE });
+    this.photos = (value ? JSON.parse(value) : []) as UserPhoto[];
+
+    // Easiest way to detect when running on the web:
+    // “when the platform is NOT hybrid, do this”
+    if (!this.platform.is('hybrid')) {
+      // Display the photo by reading into base64 format
+      for (let photo of this.photos) {
+        // Read each saved photo's data from the Filesystem
+        const readFile = await Filesystem.readFile({
+          path: photo.filepath,
+          directory: Directory.Data,
+        });
+
+        let photoData: string;
+        if (typeof readFile.data === 'string') {
+          photoData = readFile.data;
+        } else {
+          // For web, readFile.data will be a Blob
+          photoData = await this.convertBlobToBase64(readFile.data);
+        }
+
+        // Web platform only: Load the photo as base64 data
+        photo.webviewPath = `data:image/jpeg;base64,${photoData}`;
+      }
+    }
+  }
+}
--- src/app/tabs/tabs.page.ts
+++ src/app/tabs/tabs.page.ts
@@ -5,6 +5,9 @@ import { Component } from '@angular/core';
   templateUrl: 'tabs.page.html',
   styleUrls: ['tabs.page.scss']
 })
+
+
+
 export class TabsPage {
 
   constructor() {}
